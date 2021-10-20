5005bb913b7526569b498fd3eea4702bbca3959d
Sabrirois22 <92858061+Sabrirois22@users.noreply.github.com>
2021-10-20 13:35:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeColor() {
+  document.querySelectorAll('.green').forEach(element=>element.className='reset')
+  document.querySelectorAll('.red').forEach(element=>element.className='reset')
+  document.querySelectorAll('.orange').forEach(element=>element.className='reset')
+}
